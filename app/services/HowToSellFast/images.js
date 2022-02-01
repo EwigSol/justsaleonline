405af10ea63fa062797e490c282358385f071e92
EwigLife <zain@ewiglife.com>
2022-02-01 18:21:23 +0500

    removed all

--- app/services/HowToSellFast/images.js
+++
@@ -1,9 +0,0 @@
-const images = {
-  1: require("../../../assets/pick-the-right-price.png"),
-  2: require("../../../assets/use-great-photo.png"),
-  3: require("../../../assets/clear-descripion.png"),
-};
-
-export function getSellFastImages() {
-  return images;
-}
