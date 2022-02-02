04b9ea7d67c5b0e49acfd6ab12e07e61a1048fc6
EwigLife <zain@ewiglife.com>
2022-02-02 05:25:45 +0500

    social login updated

--- app/services/socialLoginConfig.js
+++ app/services/socialLoginConfig.js
@@ -1,13 +1,13 @@
 export const socialConfig = {
-  enabled: false,
+  enabled: true,
   socialPlatforms: ["facebook", "google"], // Currently we only support Facebook and Google signin. We'll add more in future
   google: {
     webClientId:
       "1009577038888-7uod8j8lcnp94u1a2so9n05g5m0gsqqd.apps.googleusercontent.com",
   },
   facebook: {
-    appID: "248887017223344",
-    appName: "Classima",
+    appID: "351236990180813",
+    appName: "Just Sale Online",
     android: {},
     iOS: {},
   },
