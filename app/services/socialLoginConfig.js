8e78892b6df62b6f2ea5483115bd98264b3d3726
EwigLife <zain@ewiglife.com>
2022-02-02 21:13:58 +0500

    updated social logins

--- app/services/socialLoginConfig.js
+++ app/services/socialLoginConfig.js
@@ -1,6 +1,6 @@
 export const socialConfig = {
   enabled: true,
-  socialPlatforms: ["facebook", "google"], // Currently we only support Facebook and Google signin. We'll add more in future
+  socialPlatforms: ["google"], // Currently we only support Facebook and Google signin. We'll add more in future
   google: {
     webClientId:
       "845992057495-tboapvm5chbdsa3ptfusdb8ip1kr4sg6.apps.googleusercontent.com",
@@ -8,7 +8,11 @@ export const socialConfig = {
   facebook: {
     appID: "351236990180813",
     appName: "Just Sale Online",
-    android: {},
-    iOS: {},
+    android: {
+      clientId: "845992057495-jjjfg7rb1qtpbcsqf0jgcbr2q72aoklp.apps.googleusercontent.com"
+    },
+    iOS: {
+      clientId: "845992057495-5ahf6cddnpep4s3tlf7ph9ctrhe6f3bm.apps.googleusercontent.com"
+    },
   },
 };
