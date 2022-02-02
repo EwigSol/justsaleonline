65942e1d2c61406f80e8a62dda8d6ab820df2ae0
EwigLife <zain@ewiglife.com>
2022-02-02 07:31:27 +0500

    asset updated

--- app/services/socialLoginConfig.js
+++ app/services/socialLoginConfig.js
@@ -3,7 +3,7 @@ export const socialConfig = {
   socialPlatforms: ["facebook", "google"], // Currently we only support Facebook and Google signin. We'll add more in future
   google: {
     webClientId:
-      "845992057495-jjjfg7rb1qtpbcsqf0jgcbr2q72aoklp.apps.googleusercontent.com",
+      "845992057495-tboapvm5chbdsa3ptfusdb8ip1kr4sg6.apps.googleusercontent.com",
   },
   facebook: {
     appID: "351236990180813",
