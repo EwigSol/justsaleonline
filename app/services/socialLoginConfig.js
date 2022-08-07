40d64a2fc526b7720b224e410c1563b475fc3726
EwigLife <zain@ewiglife.com>
2022-08-07 23:32:32 +0500

    social login sorted

--- app/services/socialLoginConfig.js
+++ app/services/socialLoginConfig.js
@@ -1,14 +1,34 @@
+// export const socialConfig = {
+//   enabled: false,
+//   socialPlatforms: ["facebook", "google"], // Currently we only support Facebook and Google signin. We'll add more in future
+//   google: {
+//     webClientId:
+//       "845992057495-tboapvm5chbdsa3ptfusdb8ip1kr4sg6.apps.googleusercontent.com",
+//   },
+//   facebook: {
+//     appID: "6786786788687",
+//     appName: "YourFBAppName",
+//     android: {},
+//     iOS: {},
+//   },
+// };
+
+
 export const socialConfig = {
-  enabled: false,
-  socialPlatforms: ["facebook", "google"], // Currently we only support Facebook and Google signin. We'll add more in future
+  enabled: true,
+  socialPlatforms: ["google", "facebook"], // Currently we only support Facebook and Google signin. We'll add more in future
   google: {
     webClientId:
-      "78687687868768-87tygi87tg8ug87t98go7987yg87f6t.apps.googleusercontent.com",
+      "845992057495-tboapvm5chbdsa3ptfusdb8ip1kr4sg6.apps.googleusercontent.com",
   },
   facebook: {
-    appID: "6786786788687",
-    appName: "YourFBAppName",
-    android: {},
-    iOS: {},
+    appID: "351236990180813",
+    appName: "Just Sale Online",
+    android: {
+      clientId: "845992057495-jjjfg7rb1qtpbcsqf0jgcbr2q72aoklp.apps.googleusercontent.com"
+    },
+    iOS: {
+      clientId: "845992057495-5ahf6cddnpep4s3tlf7ph9ctrhe6f3bm.apps.googleusercontent.com"
+    },
   },
 };
