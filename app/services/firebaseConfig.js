466372c7a02b4bc314042eed7eaffdafe3bcd0e0
EwigLife <96788793+syedzainqadri@users.noreply.github.com>
2023-01-29 07:20:17 +0500

    firebase config updated

--- app/services/firebaseConfig.js
+++ app/services/firebaseConfig.js
@@ -1,12 +1,12 @@
 export const firebaseConfig = {
   enabled: false,
   config: {
-    apiKey: "h76htnh7N77H6nhnm9079mo9887mo",
-    authDomain: "yourapp-otp.firebaseapp.com",
-    projectId: "yourapp-otp",
-    storageBucket: "yourapp-otp.appspot.com",
-    messagingSenderId: "786675766887",
-    appId: "1:786675766887:web:7h6tnyhubrg765hni7m",
-    measurementId: "G-H7NY77H788OU",
+    apiKey: "AIzaSyCM9loQN_cO8mTuPEZf6hn6HOo0U_XH1Ik",
+  authDomain: "justsaleonline-2022app.firebaseapp.com",
+  projectId: "justsaleonline-2022app",
+  storageBucket: "justsaleonline-2022app.appspot.com",
+  messagingSenderId: "845992057495",
+  appId: "1:845992057495:web:b269ba65321ab5bf8367d2",
+  measurementId: "G-HJNHK866WW"
   },
 };
